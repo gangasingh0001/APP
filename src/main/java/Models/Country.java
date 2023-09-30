db83828e7d197ed3d0bdd9c1cfe6e72c2d7ec4b0
Yang <shangmo@boqidembp.wireless.concordia.ca>
2023-09-30 17:23:54 -0400

    javadoc for country class in Models package

--- src/main/java/Models/Country.java
+++ src/main/java/Models/Country.java
@@ -3,12 +3,38 @@ package Models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a country class which manages information of all the countries on the map
+ */
 public class Country {
+
+    /**
+     * country ID
+     */
     private int d_id;
+
+    /**
+     * country name
+     */
     private String d_name;
+
+    /**
+     * the continent id which this country belongs to
+     */
     private int d_continentId;
+
+    /**
+     * armies that deployed each term to this country
+     */
     private int armies;
 
+    /**
+     * This is a constructor of the country class
+     *
+     * @param p_id country ID
+     * @param p_name country name
+     * @param p_continentId continent ID this country belongs to
+     */
     public Country(int p_id, String p_name, int p_continentId) {
         this.d_id = p_id;
         this.d_name = p_name;
@@ -16,27 +42,51 @@ public class Country {
         this.armies = 0;
     }
 
+    /**
+     * getter method to get country ID
+     * @return country ID
+     */
     public int getId() {
         return d_id;
     }
 
+    /**
+     * getter method to get country name
+     * @return country name
+     */
     public String getName() {
         return d_name;
     }
 
+    /**
+     * getter method to get continent ID of this country
+     * @return continent ID
+     */
     public int getContinentId() {
         return d_continentId;
     }
 
+    /**
+     * print method to print country name
+     * @return country name
+     */
     @Override
     public String toString() {
         return d_name;
     }
 
+    /**
+     * setter method to set number of armies to this country
+     * @param numberOfArmies number of armies assigned to this country
+     */
     public void setD_Armies(int numberOfArmies) {
         this.armies = numberOfArmies;
     }
 
+    /**
+     * getter method to get number of armies
+     * @return number of armies in this country
+     */
     public int getD_Armies() {
         return armies;
     }
