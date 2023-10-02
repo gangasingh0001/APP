0e7f2d9ecda2fde74a243ec30a92134cdf755af8
GangaSingh <46618088+gangasingh0001@users.noreply.github.com>
2023-10-02 16:39:34 -0400

    Added test files

---
+++ src/test/java/Controllers/GameEngineControllerTest.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameEngineControllerTest {
+
+    @Test
+    void mapLoader() {
+    }
+
+    @Test
+    void addRemovePlayer() {
+    }
+
+    @Test
+    void assignCountries() {
+    }
+}
---
+++ src/test/java/Services/MapServiceTest.java
@@ -0,0 +1,16 @@
+package Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapServiceTest {
+
+    @Test
+    void loadData() {
+    }
+
+    @Test
+    void validateGraph() {
+    }
+}
