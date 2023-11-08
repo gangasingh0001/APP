83b18d32055e8a11dfe453abe8c3a424b8219ada
Sun Qimeng <sun19969178yz@163.com>
2023-11-08 17:36:31 -0500

    jdoc for orders packgage

--- src/main/java/Orders/Bomb.java
+++ src/main/java/Orders/Bomb.java
@@ -1,15 +1,16 @@
 package Orders;
 
 import Models.*;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
  * This is a bomb class which implements the bomb order card
  */
 public class Bomb implements IOrders {
+    /**
+     * the hashmap between Country and player who owned this country
+     */
     private  HashMap<Country, Player> d_playerOwnedCountriesMap;
     /**
      * worldmap variable
@@ -23,20 +24,20 @@ public class Bomb implements IOrders {
      * The name of country the armies deployed to
      */
     private String d_targetCountryName ;
-
+    /**
+     * the player creating this order
+     */
     private Player d_player;
     /**
      * Parameterized Constructor for bomb card
      */
-    public Bomb(String p_sourceConuntryName, Player p_player, IWorldMap p_worldMap,HashMap<Country, Player> p_playerOwnedCountriesMap){
-//        Card card = new Card();
-//        card.setCardType(CardType.BOMB);
+    public Bomb(String p_sourceConuntryName, Player p_player, IWorldMap p_worldMap,HashMap<Country, Player> p_playerOwnedCountriesMap)
+    {
         d_targetCountryName = p_sourceConuntryName;
         d_player = p_player;
         d_worldMap=p_worldMap;
         d_playerOwnedCountriesMap=p_playerOwnedCountriesMap;
     }
-
     /**
      * override method of execute orders from players
      * bomb method to check conditions, if all met
@@ -44,18 +45,16 @@ public class Bomb implements IOrders {
      */
     @Override
     public void execute() {
-
-        //while (!d_player.getD_PlayerCards().isEmpty()){
             if (valid()){
                 int l_newArmies = d_TargetCountry.getD_Armies() / 2;
                 d_TargetCountry.setD_Armies(l_newArmies);
                 d_player.removeCard(CardType.BOMB);
-                //break;
             }
-
-        //}
     }
-
+    /**
+     * check if the order is valid
+     * @return true if the current order is valid,else return false
+     */
     @Override
     public boolean valid() {
 
@@ -86,25 +85,20 @@ public class Bomb implements IOrders {
         return true;
     }
 
-
-
-
-    /**
-     * boolean method to check the game state
-     * @param p_gameState show the states of game
-     * @return if it's a valid game state
-     */
-
-//    public boolean valid(int p_gameState) {
-//        return false;
-//    }
-
     /**
      * override method to print the order from players
      */
     @Override
     public void printOrder() {
 
+        if (valid())
+        {
+            System.out.println("the Player "+d_player.getD_playerName()+" will bomb "+d_targetCountryName);
+        }
+        else
+        {
+            System.out.println("the current order is invalid "+"the Player "+d_player.getD_playerName()+" will bomb "+d_targetCountryName);
+        }
     }
 
     /**
