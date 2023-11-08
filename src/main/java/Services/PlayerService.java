361261e2394baaaa1da414b0ad1850dc3bc3a609
Yang <caoyangtommy@hotmail.com>
2023-11-08 15:47:57 -0500

    loggings added for GameEngineController and PlayerService classes

--- src/main/java/Services/PlayerService.java
+++ src/main/java/Services/PlayerService.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.logging.Logger;
 
 /**
  * a class used to store players and manipulate players
@@ -27,18 +28,42 @@ public class PlayerService implements IPlayerService{
     /**
      * Used to store all the players in the game
      */
+
+    /**
+     * get logging records
+     */
+    private Logger logger;
+
     //TODO: Create hashmap for storing countries owned by player
+    /**
+     * hashmap for storing countries owned by player
+     */
     HashMap<Country, Player> d_playerOwnedCountriesMap;
 
+    /**
+     * a list of players
+     */
     private ArrayList<Player> d_players = new ArrayList<>();
 
     /**
-     * The constructor for PlayerService
-     *
+     *  constructor for PlayerService with logging function
+     * @param logger get logging records
      * @param p_mapService used to initial d_d_mapService
      * @param p_worldMap used to initial d_d_worldMap
      */
-    public PlayerService(IMapService p_mapService,IWorldMap p_worldMap) {
+    public PlayerService(Logger logger,IMapService p_mapService, IWorldMap p_worldMap) {
+        this.logger = logger;
+        d_mapService = p_mapService;
+        d_worldMap = p_worldMap;
+        d_playerOwnedCountriesMap = new HashMap<Country, Player>();
+    }
+
+    /**
+     * default constructor for PlayerService
+     * @param p_mapService used to initial d_d_mapService
+     * @param p_worldMap used to initial d_d_worldMap
+     */
+    public PlayerService(IMapService p_mapService, IWorldMap p_worldMap) {
         d_mapService = p_mapService;
         d_worldMap = p_worldMap;
         d_playerOwnedCountriesMap = new HashMap<Country, Player>();
@@ -81,6 +106,7 @@ public class PlayerService implements IPlayerService{
         }
         if(l_playerToRemoveObj!=null) {
             this.d_players.remove(l_playerToRemoveObj);
+            logger.severe("Player " + l_playerToRemoveObj.getD_playerName() + " removed successfully.");
             return true;
         }
         return false;
@@ -90,10 +116,10 @@ public class PlayerService implements IPlayerService{
      * Assign all the countries to all players as far as fair
      *
      */
-
     public void assignCountries() {
         List<Country> l_countryList = d_worldMap.getCountries();
         Collections.shuffle(l_countryList);
+        logger.severe("The list of countries is shuffled...");
         int currentItemIndex = 0;
         int n = this.d_players.size(); // Number of arrays to distribute items into
         int itemsPerArray = l_countryList.size() / n;
@@ -101,6 +127,7 @@ public class PlayerService implements IPlayerService{
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < itemsPerArray; j++) {
                 d_playerOwnedCountriesMap.put(l_countryList.get(currentItemIndex),this.d_players.get(i));
+                logger.severe(l_countryList.get(currentItemIndex).toString() + " is assigned to " + this.d_players.get(i).getD_playerName());
 //                this.d_players.get(i).addCountriesOwned(l_countryList.get(currentItemIndex));
 //                l_countryList.get(currentItemIndex).setD_ownedBy(this.d_players.get(i));
                 currentItemIndex++;
@@ -110,6 +137,7 @@ public class PlayerService implements IPlayerService{
         // Distribute any remaining items
         while (currentItemIndex < l_countryList.size()) {
             d_playerOwnedCountriesMap.put(l_countryList.get(currentItemIndex),this.d_players.get(currentItemIndex % n));
+            logger.severe(l_countryList.get(currentItemIndex).toString() + " is assigned to " + this.d_players.get(currentItemIndex % n).getD_playerName());
 //            this.d_players.get(currentItemIndex % n).addCountriesOwned(l_countryList.get(currentItemIndex));
 //            l_countryList.get(currentItemIndex).setD_ownedBy(this.d_players.get(currentItemIndex % n));
             currentItemIndex++;
@@ -124,6 +152,7 @@ public class PlayerService implements IPlayerService{
         for (Map.Entry<Country, Player> entry : d_playerOwnedCountriesMap.entrySet()) {
             Country country=entry.getKey();
             country.setD_NeutralCountry(false);
+            logger.severe("Set country " + country.getName() + " to not neutral...");
         }
 
         for(Player player : this.d_players) {
@@ -135,11 +164,16 @@ public class PlayerService implements IPlayerService{
                     if(!player.getD_PlayerCards().isEmpty() && player.checkIfCardExists(CardType.REINFORCEMENT)) {
                         System.out.println(player.getD_playerName() + ": Please enter Yes to use "+ CardType.REINFORCEMENT + " card");
                         l_commandEntered = l_reader.readLine();
-                        if(l_commandEntered.equalsIgnoreCase("yes")) defaultNumberOfArmies+=5;
+                        if(l_commandEntered.equalsIgnoreCase("yes")) {
+                            defaultNumberOfArmies+=5;
+                            logger.severe("Reinforcement card is used...Armies added by 5!");
+                        }
                         player.removeCard(CardType.REINFORCEMENT);
+                        logger.severe("Reinforcement card is removed！");
                     }
                     System.out.println(player.getD_playerName() + ": Please enter Deploy order or type 'exit' to quit");
                     l_commandEntered = l_reader.readLine();
+                    logger.severe(l_commandEntered);
                 } catch (IOException l_ioException) {
                     l_ioException.printStackTrace();
                 }
@@ -151,7 +185,9 @@ public class PlayerService implements IPlayerService{
                     int numOfArmiesToDeploy = Integer.parseInt(l_command.getL_secondParameter());
                     defaultNumberOfArmies = defaultNumberOfArmies - numOfArmiesToDeploy;
                     player.getD_orderList().add(new Deploy(numOfArmiesToDeploy, l_command.getL_firstParameter(), countryName,player,d_playerOwnedCountriesMap));
+                    logger.severe("Current command is: Deploy " + numOfArmiesToDeploy + " armies to country " + l_command.getL_firstParameter() + " " + countryName + " from player " + player.getD_playerName());
                 } else if (l_command.getL_rootCommand().equals(ApplicationConstants.EXIT)) {
+                    logger.severe("Exit current phase...");
                     break;
                 }
             } // Creation of deploy commands completed
@@ -170,6 +206,7 @@ public class PlayerService implements IPlayerService{
                         l_commandEntered = l_reader.readLine();
                         if(l_commandEntered.equalsIgnoreCase("yes")) {
                             System.out.println(player.getD_playerName() + ": Please enter command for "+ player.getD_PlayerCards().get(0).getCardType().name() + " card");
+                            logger.severe(player.getD_playerName() + " selects card: " + player.getD_PlayerCards().get(0).getCardType().name());
                             l_commandEntered = l_reader.readLine();
                             //TODO: validate the entered card command
                             Commands l_command = new Commands(l_commandEntered);
@@ -178,18 +215,22 @@ public class PlayerService implements IPlayerService{
                             String numOfArmies = l_command.getL_thirdParameter();
                             if(player.getD_PlayerCards().get(0).getCardType().equals(CardType.AIRLIFT)) {
                                 Airlift airlift = new Airlift(Integer.parseInt(numOfArmies),d_worldMap.findCountryNameById(Integer.parseInt(targetCountryID)),d_worldMap.findCountryNameById(Integer.parseInt(sourceCountryID)),player,d_playerOwnedCountriesMap);
+                                logger.severe(player.getD_playerName() + " is using Airlift card!");
                                 airlift.execute();
                             } else if(player.getD_PlayerCards().get(0).getCardType().equals(CardType.BLOCKADE)) {
                                 //TODO: Add blockade functionality
                                 Blockade blockade = new Blockade(d_worldMap.findCountryNameById(Integer.parseInt(sourceCountryID)),player,d_playerOwnedCountriesMap);
+                                logger.severe(player.getD_playerName() + " is using Blockade card!");
                                 blockade.execute();
                             } else if(player.getD_PlayerCards().get(0).getCardType().equals(CardType.BOMB)) {
                                 //TODO: Add Bomb functionality
                                 Bomb bomb = new Bomb(d_worldMap.findCountryNameById(Integer.parseInt(sourceCountryID)),player,d_playerOwnedCountriesMap);
+                                logger.severe(player.getD_playerName() + " is using Bomb card!");
                                 bomb.execute();
                             } else if(player.getD_PlayerCards().get(0).getCardType().equals(CardType.DIPLOMACY)) {
                                 //TODO: Add Negotiate/Diplomacy functionality
                                 Diplomacy diplomacy = new Diplomacy(l_command.getL_firstParameter());
+                                logger.severe(player.getD_playerName() + " is using Diplomacy card!");
                                 diplomacy.execute();
                             }
                         }
@@ -206,7 +247,9 @@ public class PlayerService implements IPlayerService{
                     int numOfArmiesToDeploy = Integer.parseInt(l_command.getL_thirdParameter());
                     defaultNumberOfArmies = defaultNumberOfArmies - numOfArmiesToDeploy;
                     player.getD_orderList().add(new Advance(countryNameFrom, countryNameTo, numOfArmiesToDeploy,player,d_worldMap,d_playerOwnedCountriesMap));
+                    logger.severe("Current command is: Advance " + numOfArmiesToDeploy + " armies to country ID " + countryNameTo + " from player " + player.getD_playerName() + "'s country ID " + countryNameFrom);
                 } else if (l_command.getL_rootCommand().equals(ApplicationConstants.EXIT)) {
+                    logger.severe("Exit current phase...");
                     break;
                 }
             }
