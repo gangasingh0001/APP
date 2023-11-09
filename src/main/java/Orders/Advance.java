b9836acaf6d6322e5d9c8d36f4d341a3ce2a8717
unknown <caoyangtommy@hotmail.com>
2023-11-08 21:53:11 -0500

    logging records for Advance and Airlift classes

--- src/main/java/Orders/Advance.java
+++ src/main/java/Orders/Advance.java
@@ -8,6 +8,7 @@ import Services.IPlayerService;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Logger;
 
 /**
  * This is the advance class which implements IOrders interface
@@ -23,6 +24,11 @@ public class Advance implements IOrders{
     private Country d_targetCountry;
     private HashMap<Country,Player> d_countryOwnerMap;
 
+    /**
+     * get logging records
+     */
+    private Logger d_logger;
+
     public Advance(String p_sourceConuntryName, String p_targetCountryName, int p_numberOfArmiesToDeploy, Player p_SourcePlayer, IWorldMap p_WorldMap, HashMap<Country,Player> p_countryOwnerMap)
     {
         d_numberOfArmiesToAdvance = p_numberOfArmiesToDeploy;
@@ -31,6 +37,27 @@ public class Advance implements IOrders{
         d_SourcePlayer = p_SourcePlayer;
         d_WorldMap = p_WorldMap;
         d_countryOwnerMap = p_countryOwnerMap;
+    }
+
+    /**
+     * Advance class constructor with logging records
+     * @param p_sourceConuntryName source country
+     * @param p_targetCountryName target country
+     * @param p_numberOfArmiesToDeploy number of arimes
+     * @param p_SourcePlayer source player
+     * @param p_WorldMap world map
+     * @param p_countryOwnerMap country owned map
+     * @param p_logger logging records
+     */
+    public Advance(String p_sourceConuntryName, String p_targetCountryName, int p_numberOfArmiesToDeploy, Player p_SourcePlayer, IWorldMap p_WorldMap, HashMap<Country,Player> p_countryOwnerMap, Logger p_logger)
+    {
+        d_numberOfArmiesToAdvance = p_numberOfArmiesToDeploy;
+        d_targetCountryName = p_targetCountryName;
+        d_sourceConuntryName = p_sourceConuntryName;
+        d_SourcePlayer = p_SourcePlayer;
+        d_WorldMap = p_WorldMap;
+        d_countryOwnerMap = p_countryOwnerMap;
+        d_logger = p_logger;
     }
     /**
      * number of armies assigned each term
@@ -47,17 +74,7 @@ public class Advance implements IOrders{
      */
     private String d_targetCountryID ;
 
-    /**
-     * constructor class of Advance
-     * @param p_numberOfArmiesToDeploy parameter of  number of armies to assign
-     * @param p_targetCountryID  parameter of target country ID
-     * @param p_targetCountryName parameter of target country name
-     */
-//    public Advance(int p_numberOfArmiesToDeploy,String p_targetCountryID, String p_targetCountryName) {
-//        this.d_numberOfArmiesToDeploy = p_numberOfArmiesToDeploy;
-//        this.d_targetCountryName = p_targetCountryName;
-//        this.d_targetCountryID = p_targetCountryID;
-//    }
+
 
     /**
      * override method of execute orders from players
@@ -75,27 +92,41 @@ public class Advance implements IOrders{
 //                    d_sourceCountry = country;
 //                }
 //            }
+            d_logger.severe("Advance order starts!");
             d_sourceCountry.setD_Armies(d_sourceCountry.getD_Armies()-this.d_numberOfArmiesToAdvance);
+            d_logger.severe(d_sourceCountry.getName() + " left " + (d_sourceCountry.getD_Armies()-this.d_numberOfArmiesToAdvance) + " armies in its own territory.");
             if(d_countryOwnerMap.get(d_targetCountry).equals(d_SourcePlayer)) {
+                d_logger.severe("Target to this player's own country, advance order changes to deploy!");
                  d_targetCountry.setD_Armies(d_targetCountry.getD_Armies()+this.d_numberOfArmiesToAdvance);
             } else {
                 while (this.d_numberOfArmiesToAdvance != 0 && d_targetCountry.getD_Armies() != 0) {
+                    d_logger.severe("Attack begins!");
                     Random random = new Random();
                     int l_attcakRandom = random.nextInt(0, 10);
                     int l_defenceRandom = random.nextInt(0, 10);
-                    if (l_attcakRandom <= 5) this.d_numberOfArmiesToAdvance--;
-                    if (l_defenceRandom <= 4) d_targetCountry.setD_Armies(d_targetCountry.getD_Armies()-1);
+                    if (l_attcakRandom <= 5) {
+                        d_logger.severe(this.d_sourceCountry.getName() + " loses one army by attacking!");
+                        this.d_numberOfArmiesToAdvance--;
+                    }
+                    if (l_defenceRandom <= 4) {
+                        d_logger.severe(d_targetCountry.getName() + " loses one army by defending!");
+                        d_targetCountry.setD_Armies(d_targetCountry.getD_Armies()-1);
+                    }
+
                 }
 
                 if (d_targetCountry.getD_Armies() == 0 && this.d_numberOfArmiesToAdvance!=0) {
                     if(!d_targetCountry.isD_NeutralCountry()) {
+                        d_logger.severe(d_targetCountry.getName() + " defeated, now this country owned by " + d_SourcePlayer.getD_playerName());
                         d_countryOwnerMap.put(d_targetCountry, d_SourcePlayer);
                         d_targetCountry.setD_Armies(this.d_numberOfArmiesToAdvance);
+                        d_logger.severe(d_SourcePlayer.getD_playerName() + " newly owned country " + d_targetCountry.getName() + " now has " + this.d_numberOfArmiesToAdvance + " armies.");
                         Player d_TargetPlayer = d_countryOwnerMap.get(d_targetCountry);
                         d_TargetPlayer.removeAcquiredCountry(d_targetCountry);
                         d_SourcePlayer.addAcquiredCountry(d_targetCountry);
                         d_countryOwnerMap.put(d_targetCountry, d_SourcePlayer);
                     } else {
+                        d_logger.severe("Now " + d_sourceCountry.getName() + " remains " + (d_sourceCountry.getD_Armies()+this.d_numberOfArmiesToAdvance) + " armies.");
                         d_sourceCountry.setD_Armies(d_sourceCountry.getD_Armies()+this.d_numberOfArmiesToAdvance);
                         //d_countryOwnerMap.remove(d_targetCountry);
                     }
@@ -110,6 +141,7 @@ public class Advance implements IOrders{
      */
     @Override
     public boolean valid() {
+        d_logger.severe("Check if Advance order is valid...");
         boolean sourceCountryFind=false;
         for (Map.Entry<Country, Player> entry : d_countryOwnerMap.entrySet()) {
             Country temp=entry.getKey();
@@ -119,7 +151,10 @@ public class Advance implements IOrders{
                 break;
             }
         }
-        if (!sourceCountryFind){;System.out.println("source country is not exist");return false;}
+        if (!sourceCountryFind){
+            d_logger.severe("source country is not exist");
+            System.out.println("source country is not exist");
+            return false;}
         boolean targetCountryFind=false;
         for (Map.Entry<Country, Player> entry : d_countryOwnerMap.entrySet())
         {
@@ -130,7 +165,11 @@ public class Advance implements IOrders{
                 break;
             }
         }
-        if (!targetCountryFind){System.out.println("target country is not exist");return false;}
+        if (!targetCountryFind){
+            d_logger.severe("target country is not exist");
+            System.out.println("target country is not exist");
+            return false;
+        }
 
 
      if(!d_WorldMap.getNeighborsOfCountry(d_targetCountry).contains(d_sourceCountry))
@@ -139,7 +178,8 @@ public class Advance implements IOrders{
      }
         if (d_SourcePlayer.getD_diplomacyWith().contains(d_countryOwnerMap.get(d_targetCountry)))
         {
-            System.out.println("the advance order can is invalid, because two player has diplomacy");
+            d_logger.severe("The advance order is invalid, because two players have diplomacy");
+            System.out.println("The advance order is invalid, because two players have diplomacy");
             return false;
         }
      if(d_countryOwnerMap.get(d_sourceCountry).equals(d_SourcePlayer)&&this.d_numberOfArmiesToAdvance<=d_sourceCountry.getD_Armies()-1)
@@ -148,12 +188,14 @@ public class Advance implements IOrders{
      }
      else if (!d_countryOwnerMap.get(d_sourceCountry).equals(d_SourcePlayer))
      {
+         d_logger.severe("Current Country is not belong to Source Player");
          System.out.println("Current Country is not belong to Source Player");
          return false;
      }
      else
      {
-         System.out.println("We Do not Have Enough Arimes yo Attack");
+         d_logger.severe("We Do not Have Enough Armies to Attack");
+         System.out.println("We Do not Have Enough Armies to Attack");
          return false;
      }
 
@@ -163,10 +205,15 @@ public class Advance implements IOrders{
      */
     @Override
     public void printOrder() {
-        if (!valid())System.out.println("the current order is invalid");
+        d_logger.severe("Advance order validating...");
+        if (!valid()){
+            d_logger.severe("The current order is invalid");
+            System.out.println("The current order is invalid");
+        }
         else
         {
-            System.out.println(d_SourcePlayer.getD_playerName()+" will attack country "+d_targetCountry.getName()+" form country "+d_sourceCountry.getName()+" with the number of arimes "+d_numberOfArmiesToAdvance);
+            d_logger.severe(d_SourcePlayer.getD_playerName()+" will attack country "+d_targetCountry.getName()+" form country "+d_sourceCountry.getName()+" with the number of armies "+d_numberOfArmiesToAdvance);
+            System.out.println(d_SourcePlayer.getD_playerName()+" will attack country "+d_targetCountry.getName()+" form country "+d_sourceCountry.getName()+" with the number of armies "+d_numberOfArmiesToAdvance);
         }
     }
 
--- src/main/java/Orders/Airlift.java
+++ src/main/java/Orders/Airlift.java
@@ -8,6 +8,7 @@ import Services.CountryService;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class Airlift implements IOrders
 {
@@ -19,6 +20,11 @@ public class Airlift implements IOrders
     private Country d_targetCountry;
     private HashMap<Country,Player> d_countryOwnerMap;
 
+    /**
+     * get logging records
+     */
+    private Logger d_logger;
+
     public Airlift(int p_numberOfArmiesToDeploy, String p_targetCountryName, String p_sourceConuntryName, Player p_sourcePlayer, HashMap<Country,Player> p_countryOwnerMap) {
         d_numberOfArmiesToAdvance=p_numberOfArmiesToDeploy;
         d_targetCountryName=p_targetCountryName;
@@ -27,6 +33,27 @@ public class Airlift implements IOrders
         d_countryOwnerMap=p_countryOwnerMap;
         d_SourcePlayer.removeCard(CardType.AIRLIFT);
     }
+
+    /**
+     * constructor for card airlift with logging reocrds
+     * @param p_numberOfArmiesToDeploy number of armies to deploy
+     * @param p_targetCountryName target country
+     * @param p_sourceConuntryName source country name
+     * @param p_sourcePlayer source player
+     * @param p_countryOwnerMap country owned map
+     * @param p_logger logging records
+     */
+    public Airlift(int p_numberOfArmiesToDeploy, String p_targetCountryName, String p_sourceConuntryName, Player p_sourcePlayer, HashMap<Country,Player> p_countryOwnerMap, Logger p_logger) {
+        d_numberOfArmiesToAdvance=p_numberOfArmiesToDeploy;
+        d_targetCountryName=p_targetCountryName;
+        d_sourceConuntryName=p_sourceConuntryName;
+        d_SourcePlayer=p_sourcePlayer;
+        d_countryOwnerMap=p_countryOwnerMap;
+        d_SourcePlayer.removeCard(CardType.AIRLIFT);
+        d_logger = p_logger;
+    }
+
+
     @Override
     public void execute() {
 
@@ -41,6 +68,7 @@ public class Airlift implements IOrders
 //                    d_sourceCountry = country;
 //                }
 //            }
+            d_logger.severe("Airlift card is playing!");
             d_sourceCountry.setD_Armies(d_sourceCountry.getD_Armies()-this.d_numberOfArmiesToAdvance);
             if(d_countryOwnerMap.get(d_targetCountry).equals(d_SourcePlayer))
             {
@@ -69,6 +97,7 @@ public class Airlift implements IOrders
     }
     @Override
     public boolean valid() {
+        d_logger.severe("Check if Airlift card is valid...");
         boolean sourceCountryFind=false;
         for (Map.Entry<Country, Player> entry : d_countryOwnerMap.entrySet())
         {
@@ -79,7 +108,10 @@ public class Airlift implements IOrders
                 break;
             }
         }
-        if (!sourceCountryFind){;System.out.println("source country is not exist");return false;}
+        if (!sourceCountryFind){
+            d_logger.severe("source country is not exist");
+            System.out.println("source country is not exist");
+            return false;}
         boolean targetCountryFind=false;
         for (Map.Entry<Country, Player> entry : d_countryOwnerMap.entrySet())
         {
@@ -90,10 +122,14 @@ public class Airlift implements IOrders
                 break;
             }
         }
-        if (!targetCountryFind){System.out.println("target country is not exist");return false;}
+        if (!targetCountryFind){
+            d_logger.severe("target country is not exist");
+            System.out.println("target country is not exist");
+            return false;}
         if (d_SourcePlayer.getD_diplomacyWith().contains(d_countryOwnerMap.get(d_targetCountry)))
         {
-            System.out.println("the advance order can is invalid, because two player has diplomacy");
+            d_logger.severe("The advance order is invalid, because two players have diplomacy");
+            System.out.println("The advance order is invalid, because two players have diplomacy");
             return false;
         }
         if(d_countryOwnerMap.get(d_sourceCountry).equals(d_SourcePlayer)&&this.d_numberOfArmiesToAdvance<=d_sourceCountry.getD_Armies()-1)
@@ -102,21 +138,28 @@ public class Airlift implements IOrders
         }
         else if (!d_countryOwnerMap.get(d_sourceCountry).equals(d_SourcePlayer))
         {
+            d_logger.severe("Current Country is not belong to Source Player");
             System.out.println("Current Country is not belong to Source Player");
             return false;
         }
         else
         {
-            System.out.println("We Do not Have Enough Arimes yo Attack");
+            d_logger.severe("We Do not Have Enough Armies yo Attack");
+            System.out.println("We Do not Have Enough Armies yo Attack");
             return false;
         }
 
     }
     @Override
     public void printOrder() {
-        if (!valid())System.out.println("the current order is invalid");
+        d_logger.severe("Airlift card validating...");
+        if (!valid()){
+            d_logger.severe("the current order is invalid");
+            System.out.println("the current order is invalid");
+        }
         else
         {
+            d_logger.severe(d_SourcePlayer.getD_playerName()+" will Airlift to country "+d_targetCountry.getName()+" form country "+d_sourceCountry.getName()+" with the number of arimes "+d_numberOfArmiesToAdvance);
             System.out.println(d_SourcePlayer.getD_playerName()+" will Airlift to country "+d_targetCountry.getName()+" form country "+d_sourceCountry.getName()+" with the number of arimes "+d_numberOfArmiesToAdvance);
         }
     }
